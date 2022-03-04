da05b0c897662c5383eab0c023de291c1a153e6c
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-03-04 17:09:42 +0300

    add spawn bombs and destroy bombs

---
+++ Assets/Scripts/DestroyBombs.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBombs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Border")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
