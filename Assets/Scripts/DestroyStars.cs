7c6e0fab3537315925cfe92bb042b1902081eaf5
hesrayilmaz <hesra.yilmazz@gmail.com>
2022-06-20 00:07:49 +0300

    add star particules when player collides

--- Assets/Scripts/DestroyStars.cs
+++ Assets/Scripts/DestroyStars.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DestroyStars : MonoBehaviour
 {
+    public ParticleSystem ps;
+    public SpriteRenderer sr;
+    public bool once = true;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,11 +16,31 @@ public class DestroyStars : MonoBehaviour
             Destroy(this.gameObject);
         }
 
-        if (collision.tag == "Player")
+        if(collision.tag == "Player" )
         {
+            ScoreManager.score++;
+            var em = ps.emission;
+            var dur = ps.duration;
+            
+            em.enabled = true;
+            ps.Play();
+
+            
+            Destroy(sr);
+            Invoke(nameof(DestroyObj), dur);
+            
+        }
+/*
+        if (collision.tag == "Player" )
+        {  
             Destroy(this.gameObject);
             ScoreManager.score++;
         }
+*/
+    }
 
+    void DestroyObj()
+    {
+        Destroy(this.gameObject);
     }
 }
